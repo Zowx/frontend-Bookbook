53e7b126ff834db465836973f6444aefc0da49bd
Tommy <tommy.angibaud@gmail.com>
2025-01-20 11:57:37 +0100

    [US] Adding folder to the front and tailwindcss

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='bg-slate-600 w-full'>
+            <span>
+            Coucou je suis la navbar.
+            </span>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/Pages/HomePages.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../Components/NavBar';
+
+
+const HomePage = () => {
+    return (
+        <div>
+            <Navbar />
+            Coucou je suis la Home Page
+        </div>
+    );
+};
+
+export default HomePage;
